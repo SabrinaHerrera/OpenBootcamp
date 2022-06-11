2ba6c1583c543d2407e8e183c8ffe04b359d4e2e
Sabrina <sabrinaherrera199@gmail.com>
2022-06-11 10:55:14 -0300

    Ejercicio3 Open Botcamp

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//Crear funcion con tres para metros int que se sumen
+//Llamar a la funcion desde el main y darle valores
+
+public class Main {
+    public static void main(String[] args){
+        int resultado =0;
+        resultado = suma(10,5,4);
+        System.out.println(resultado);
+    }
+
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+
+
